799535823924257982ef69ea2b911446f730907e
Felipe Castro <carloscastro1860@gmail.com>
2025-05-08 12:55:31 -0500

    create pokedex with react and typescript

---
+++ src/pages/home/layout/LayoutHome.tsx
@@ -0,0 +1,31 @@
+import Header from "../header/Header";
+import Card from "../card/Card";
+import css from "./layout.module.scss";
+import axios from "axios";
+import { URL_POKEMON } from "../../../api/apiRest";
+import { useEffect, useState } from "react";
+
+export default function LayoutHome() {
+  const [arrayPokemon, setArrayPokemon] = useState([]);
+
+  useEffect(() => {
+    const api = async () => {
+      const apiPoke = await axios.get(`${URL_POKEMON}`);
+
+      setArrayPokemon(apiPoke.data.results);
+    };
+    api();
+  }, []);
+
+  return (
+    <div className={css.layout}>
+      <Header />
+
+      <div className={css.card_content}>
+        {arrayPokemon.map((card, index) => {
+          return <Card key={index} card={card} />;
+        })}
+      </div>
+    </div>
+  );
+}
